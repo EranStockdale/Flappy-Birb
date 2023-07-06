eb3bae9ba74f6ddf094a4ac0b9ea8a237cacd0bf
Eran Stockdale <eranbd2017@outlook.com>
2023-07-06 22:02:49 +0100

    use unity input system (its nicer :))

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BirdScript : MonoBehaviour
 {
     public float flapStrength = 10;
     private LogicScript logic;
     public bool isAlive = true;
+    public PlayerInputActions playerControls;
+    private InputAction flap;
+
+    void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+
+    void OnEnable()
+    {
+        flap = playerControls.Player.Flap;
+        flap.Enable();
+        flap.performed += Flap;
+    }
+
+    void OnDisable()
+    {
+        flap.Disable(); 
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +37,16 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isAlive) {
-            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up * flapStrength;
-        }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         isAlive = false;
         logic.GameOver();
+    }
+
+    private void Flap(InputAction.CallbackContext context) {
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up * flapStrength;
     }
 }
